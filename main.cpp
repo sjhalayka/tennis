8127b7e6cd6452e44f04abb8b9efff1d5311beb9
Shawn Halayka <sjhalayka@gmail.com>
2019-03-11 16:44:30 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -197,14 +197,14 @@ void draw_objects(void)
 
 	for (size_t i = 0; i < paths.size(); i++)
 	{
-		glBegin(GL_LINE_STRIP);
-
 		if (i == path1_index)
 			glColor3f(0.0, 1.0, 0.0);
 		else if (i == path2_index)
 			glColor3f(0.0, 0.0, 1.0);
 		else
-			glColor3f(1.0, 0.0, 0.0);
+			continue;
+
+		glBegin(GL_LINE_STRIP);
 
 		for (size_t j = 0; j < paths[i].size(); j++)
 			glVertex3f(paths[i][j].x, paths[i][j].y, paths[i][j].z);
